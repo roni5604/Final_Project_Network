0336ab95f46c1f78efaf421152959b27179ae1da
roni5604 <roniro5604@gmail.com>
2023-03-04 13:18:51 +0200

    fix Http links

--- File_download.py
+++ File_download.py
@@ -14,13 +14,63 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
             with open('LecturesWebPage.html', 'rb') as f:
                 self.wfile.write(f.read())
-        elif self.path == '/download':
-            # Download the file from the other server using wget
-            url = 'http://filesamples.com/samples/document/txt/sample3.txt'
+        elif self.path.startswith('/download') and self.path != '/download':
+            if self.path == '/download/Beginner_Tutorial':
+                # Download the file from the other server using wget
+                url = 'http://filesamples.com/samples/document/txt/sample3.txt'
+                file_name = url.split('/')[-1]
+                subprocess.call(['wget', url])
+
+                # Redirect the client to the downloaded file
+                file_path = file_name
+                self.send_response(200)
+                self.send_header('Content-Disposition', f'attachment; filename={file_name}')
+                self.send_header('Content-Length', f'{os.path.getsize(file_path)}')
+                self.send_header('Content-Type', 'application/octet-stream')
+                self.end_headers()
+                with open(file_path, 'rb') as f:
+                    self.wfile.write(f.read())
+                print(f"Downloaded file {file_name} from {url}")
+            elif self.path == '/download/Junior_Tuturial':
+                # Download the file from the other server using wget
+                url = 'http://filesamples.com/samples/document/txt/sample3.txt'
+                file_name = url.split('/')[-1]
+                subprocess.call(['wget', url])
+
+                # Redirect the client to the downloaded file
+                file_path = file_name
+                self.send_response(200)
+                self.send_header('Content-Disposition', f'attachment; filename={file_name}')
+                self.send_header('Content-Length', f'{os.path.getsize(file_path)}')
+                self.send_header('Content-Type', 'application/octet-stream')
+                self.end_headers()
+                with open(file_path, 'rb') as f:
+                    self.wfile.write(f.read())
+                print(f"Downloaded file {file_name} from {url}")
+            elif self.path == '/download/Full_Tuturial':
+                # Download the file from the other server using wget
+                url = 'http://filesamples.com/samples/document/txt/sample3.txt'
+                file_name = url.split('/')[-1]
+                subprocess.call(['wget', url])
+
+                # Redirect the client to the downloaded file
+                file_path = file_name
+                self.send_response(200)
+                self.send_header('Content-Disposition', f'attachment; filename={file_name}')
+                self.send_header('Content-Length', f'{os.path.getsize(file_path)}')
+                self.send_header('Content-Type', 'application/octet-stream')
+                self.end_headers()
+                with open(file_path, 'rb') as f:
+                    self.wfile.write(f.read())
+                print(f"Downloaded file {file_name} from {url}")
+        elif self.path.startswith('/list') and self.path != '/list':
+            if self.path == '/list/Beginner_Tutorial':
+            # Ask for the list of files from the other server
+            url = 'http://localhost:12345/list/Beginner_Tutorial'
             file_name = url.split('/')[-1]
             subprocess.call(['wget', url])
 
-            # Redirect the client to the downloaded file
+            # Redirect the client to open the list of files
             file_path = file_name
             self.send_response(200)
             self.send_header('Content-Disposition', f'attachment; filename={file_name}')
@@ -30,6 +80,7 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
             with open(file_path, 'rb') as f:
                 self.wfile.write(f.read())
             print(f"Downloaded file {file_name} from {url}")
+
         return super().do_GET()
 
 Handler = MyHttpRequestHandler
