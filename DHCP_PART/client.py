bb3f6e6aaeb7ca437e09002493e4f0712279f880
elor <elorisraeli@gmail.com>
2023-03-12 12:56:03 +0200

    fix DHCP

--- DHCP_PART/client.py
+++ DHCP_PART/client.py
@@ -5,10 +5,27 @@ from scapy.layers.l2 import Ether
 
 
 # Define the MAC address and IP address of the client
-mac = "00:11:22:33:44:55"!!!!!!!!!!!!
-ip = "0.0.0.0"!!!!!!!!!!!!
+mac = "00:00:00:00:00:00"
+def mac_to_bytes(mac_addr: str) -> bytes:
+    """ Converts a MAC address string to bytes.
+    """
+    return int(mac_addr.replace(":", ""), 16).to_bytes(6, "big")
+mac_in_bytes=mac_to_bytes(mac)
 
-etrenetType="en33"!!!!!!!!!!!!!!!
+etrenetType="lo"
+
+xid=random.randint(100,100000)
+
+# Create a DHCP Discover packet
+dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff", src=mac) / \
+                IP(src="0.0.0.0", dst="255.255.255.255") / \
+                UDP(sport=68, dport=67) / \
+                BOOTP(op=1, chaddr=mac_in_bytes, xid=xid) / \
+                DHCP(options=[("message-type", "discover"), "end"])
+print("client send discover")
+# Send the DHCP Discover packet and wait for the DHCP Offer
+srp1(dhcp_discover, iface=etrenetType, timeout=0.5)
+
 
 def handler_clienet(packet):
     if DHCP in packet and packet[DHCP].options[0][1] == 2:
--- DHCP_PART/server.py
+++ DHCP_PART/server.py
@@ -1,24 +1,24 @@
 from scapy.all import *
 import random
-
+import threading
 from scapy.layers.dhcp import BOOTP, DHCP
 from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether
 
 # Define the IP range for DHCP to assign addresses from
-ip_range = "192.168.0."!!!!!!!!!
+ip_range = "192.168.75."
 
 # Define the MAC address of the server
-mac = "00:11:22:33:44:66"!!!!!!!!!
+mac = "00:0c:29:9b:51:47"
 
-etrenetType="en33"!!!!!!!!!!!!!!!
+# Define
+ethernetType= "lo"
 
 # Define the lease time for assigned IP addresses (in seconds)
-lease_time = 3600
+LEASE_TIME = 3600
 
 # Define the list of assigned IP addresses
 assigned_ips = {}
-
 
 
 # Define a function to generate a new IP address
@@ -37,19 +37,19 @@ def handle_dhcp_discover(packet):
     ip_new = get_new_ip()
 
     # Add the IP address to the list of assigned IPs
-    assigned_ips[ip_new] = {"mac": client_mac, "lease_time": lease_time}
+    assigned_ips[ip_new] = {"mac": client_mac, "lease_time": LEASE_TIME}
 
     # Create a DHCP Offer packet with the new IP address
     dhcp_offer = Ether(dst=client_mac)/ \
                  IP(src=ip_range+"130", dst="255.255.255.255")/ \
                  UDP(sport=67, dport=68)/ \
                  BOOTP(op=2, yiaddr=ip_new, siaddr=ip_range+"130", giaddr="0.0.0.0", xid=packet[BOOTP].xid,chaddr=client_mac)/ \
-                 DHCP(options=[("message-type", "offer"),("server_id", ip_range+"130"),("subnet_mask", "255.255.255.0"),("lease_time", lease_time),("router", ip_range+"2"),"end"])
+                 DHCP(options=[("message-type", "offer"), ("server_id", ip_range+"130"), ("subnet_mask", "255.255.255.0"), ("lease_time", LEASE_TIME), ("router", ip_range + "2"), "end"])
 
     print("server send offer")
     # Send the DHCP Offer packet
     time.sleep(1)
-    sendp(dhcp_offer, iface=etrenetType)
+    sendp(dhcp_offer, iface=ethernetType)
 
 # Define a function to handle DHCP Request messages
 def handle_dhcp_request(packet):
@@ -61,25 +61,38 @@ def handle_dhcp_request(packet):
     client_mac = packet[Ether].src
 
     # Check if the requested IP address is available and assigned to this client
-    print("i made in here")
-    # Create a DHCP Ack packet with the assigned IP address
-    dhcp_ack = Ether(dst=client_mac) / \
-               IP(src=ip_range + "130", dst="255.255.255.255") / \
-               UDP(sport=67, dport=68) / \
-               BOOTP(op=2, yiaddr=requested_ip, siaddr=ip_range + "130", giaddr="0.0.0.0", xid=packet[BOOTP].xid,
-                     chaddr=client_mac) / \
-               DHCP(options=[("message-type", "ack"),
-                             ("server_id", ip_range + "130"),
-                             ("subnet_mask", "255.255.255.0"),
-                             ("lease_time", assigned_ips[requested_ip]["lease_time"]),
-                             ("router", ip_range + "2"),
-                             "end"])
+    if requested_ip in assigned_ips and assigned_ips[requested_ip]["mac"] == client_mac:
+        # Create a DHCP Ack packet with the assigned IP address
+        dhcp_ack = Ether(dst=client_mac) / \
+                   IP(src=ip_range + "130", dst="255.255.255.255") / \
+                   UDP(sport=67, dport=68) / \
+                   BOOTP(op=2, yiaddr=requested_ip, siaddr=ip_range + "130", giaddr="0.0.0.0", xid=packet[BOOTP].xid,
+                         chaddr=client_mac) / \
+                   DHCP(options=[("message-type", "ack"),
+                                 ("server_id", ip_range + "130"),
+                                 ("subnet_mask", "255.255.255.0"),
+                                 ("lease_time", assigned_ips[requested_ip]["lease_time"]),
+                                 ("router", ip_range + "2"),
+                                 "end"])
 
-    print("server send ack")
+        print("server send ack")
 
         # Send the DHCP Ack packet
-        sendp(dhcp_ack, iface=etrenetType)
+        time.sleep(1)
+        sendp(dhcp_ack, iface=ethernetType)
+        print(assigned_ips)
+
+
+def packet_handler(packet):
+    if DHCP in packet and packet[DHCP].options[0][1] == 1:
+        handle_dhcp_discover(packet)
+    elif DHCP in packet and packet[DHCP].options[0][1] == 3:
+        handle_dhcp_request(packet)
+
+
 
 # Start sniffing for DHCP Discover and Request messages
-sniff(filter="udp and (port 67 or port 68)", prn=packet_handler, iface=etrenetType)
+sniff(filter="udp and (port 67 or port 68)", prn=packet_handler, iface=ethernetType)
+
+
 
