103adb61a59c37a1dc4204aa43188e25ac6498ac
roni5604 <roniro5604@gmail.com>
2023-03-12 12:53:14 +0200

    DNS side

--- RUDP_appliction/server_file_rudp.py
+++ RUDP_appliction/server_file_rudp.py
@@ -8,24 +8,26 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def main_tcp():
     # create a socket object
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # bind the socket to a public host and port
     server_socket.bind((FILES_SERVER_HOST, FILES_SERVER_PORT))
 
     # listen for incoming connections
-    server_socket.listen(1)
 
     print(f'Serving files on {FILES_SERVER_HOST}:{FILES_SERVER_PORT}...')
 
     while True:
+
         # accept incoming client connections
-        client_connection, client_address = server_socket.accept()
+        while True:
+            # get the request data in chunks
 
-        # get the request data
-        request_data = client_connection.recv(1024).decode()
 
+
+        request_data, client_address = server_socket.recvfrom(512)
+        request_data = request_data.decode()
         # parse the request data
         request_lines = request_data.split('\r\n')
         request_line = request_lines[0]
@@ -55,11 +57,7 @@ def main_tcp():
             # construct the response data for a 404 Not Found error
             response_data = b'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\nConnection: close\r\n\r\n'
 
-        # send the response data back to the client
-        client_connection.sendall(response_data)
 
-        # close the client connection
-        client_connection.close()
 
 
 if __name__ == '__main__':
